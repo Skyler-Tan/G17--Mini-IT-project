95944a0b89bfe6565e9f7203b82e400fe3c8a9d2
Thanish <thanishthevan87@gmail.com>
2025-09-25 14:54:38 +0800

    updated combined

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash, request, make_response
+from flask import Flask, render_template, redirect, url_for, flash, request, make_response, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
